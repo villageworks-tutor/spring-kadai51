55492faae3696522299908ce2a5cd80a7539ef60
villageworks <villageworks@gmail.com>
2023-11-15 23:04:21 +0900

    ver.0.01: 商品名と価格の組み合わせ検索の実装

--- src/main/java/com/example/demo/repository/ItemRepository.java
+++ src/main/java/com/example/demo/repository/ItemRepository.java
@@ -9,13 +9,20 @@ import com.example.demo.entity.Item;
 public interface ItemRepository extends JpaRepository<Item, Integer> {
 
 	// 価格の安い順（昇順）に商品を取得する
+	// SELECT * FROM items ORDER BY price
 	List<Item> findAllByOrderByPriceAsc();
 
 	// 上限金額以下の価格の商品を取得する
+	// SELECT * FROM items WHERE price <= ?
 	List<Item> findByPriceLessThanEqual(Integer maxPrice);
 
 	// nameフィールドで部分一致検索を実行する
+	// SELECT * FROM items WHERE name LIKE ?
 	List<Item> findByNameContaining(String keyword);
+
+	// キーワードと価格の組み合わせ検索
+	// SELECT * FROM items WHERE name LIKE ? AND price <= ?
+	List<Item> findByNameContainingAndPriceLessThanEqual(String keyword, Integer maxPrice);
 
 
 }
